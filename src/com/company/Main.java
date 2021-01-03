f825bf5e632771fff85bff443ac8c292f76855ac
quincy.williams <quincy.williams@hoverstate.com>
2021-01-03 10:56:36 -0800

    udpate

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var scanner = new Scanner(System.in);
+
+        System.out.print("Enter a numeric value: ");
+        var number1 = scanner.nextDouble();
+
+        System.out.print("Enter a numeric value: ");
+        var number2 = scanner.nextDouble();
+
+        var sum = number1 / number2;
+        System.out.println("The answer is: "+ sum);
     }
 }
