d9bd9d6aca911a77996b99025aab54ae1bdfaa98
quincy.williams <quincy.williams@hoverstate.com>
2021-01-03 17:05:13 -0800

    additional changes to main via more conditions

---
+++ src/com/company/MoreConditions.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class MoreConditions {
+
+    public static void main(String[] args) {
+// create scanner object and declare a variable to input a month number
+
+//        var scanner = new Scanner(System.in);
+//        System.out.print("Enter a month number (1-12): ");
+//        var monthNumber = scanner.nextInt();
+
+/* get local datetime stamp via a variable from the local\datetime.now class
+   get month number from the expression now.getMonthValue
+ */
+        var now = LocalDateTime.now();
+        var monthNumber = now.getMonthValue();
+
+// declare string variable; conditional logic will assign
+
+        String message;
+
+/* print message based upon input:
+   a month number between 1 and 12
+   a month number less than or equal to 3 is 1st quarter
+   a month number less than or equal to 6 is 2nd quarter
+   a month number greater than 6 but less than 12 is second half of year
+ */
+//        if (monthNumber < 1 || monthNumber > 12) {
+//            message = "That isn't a valid month!";
+//        } else
+        if (monthNumber <= 3) {
+            message = "That's in Quarter 1!";
+        } else if (monthNumber <= 6) {
+            message = "That's in Quarter 2!";
+        } else {
+            message = "That isn't in the first half of the year";
+        }
+        System.out.println(message);
+    }
+}
