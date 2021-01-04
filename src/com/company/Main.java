6f10a84eda535b463a33d9ad8a9ba1362493140e
quincy.williams <quincy.williams@hoverstate.com>
2021-01-03 16:41:30 -0800

    my changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+// create scanner object and declare a variable to input a month number
 
         var scanner = new Scanner(System.in);
         System.out.print("Enter a month number (1-12): ");
         var monthNumber = scanner.nextInt();
 
+// declare string variable; conditional logic will assign
+
         String message;
+
+/* print message based upon input:
+   a month number between 1 and 12
+   a month number less than or equal to 3 is 1st quarter
+   a month number less than or equal to 6 is 2nd quarter
+   a month number greater than 6 but less than 12 is second half of year
+ */
         if (monthNumber < 1 || monthNumber > 12) {
             message = "That isn't a valid month!";
-        } else if (monthNumber <= 3)  {
+        } else if (monthNumber <= 3) {
             message = "That's in Quarter 1!";
-        } else if (monthNumber <=6) {
+        } else if (monthNumber <= 6) {
             message = "That's in Quarter 2!";
         } else {
             message = "That isn't in the first half of the year";
         }
+        System.out.println(message);
     }
 }
